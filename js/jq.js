a4b2d136f2e3f282454159826bc4687217c9f3d0
DrVali <ahmadi.world@gmail.com>
2024-09-09 00:35:48 +0330

    Update jq.js

--- js/jq.js
+++ js/jq.js
@@ -1,1 +1,23 @@
+ function about_age() {
+    let lbl = document.getElementById("result");
 
+     let num1 = document.getElementById("num").value;
+
+          if(!isNaN(num1)){
+              if( num1 >  17)
+              {
+               lbl.innerHTML= "You are an adult.";
+              }
+              else if( num1 > 12 && num1 < 18){
+                 console.log("dllkllkd");
+               lbl.innerHTML= "You are a teenager.";
+              }
+                 else if( num1 < 13){
+
+               lbl.innerHTML= "You are a child.";
+              }
+          }
+        else{
+            alert(`Please Insert Your age Number.`);
+        }
+}
